ecc48a45d7dcc0c684963995a19ead1a0143ed25
agamswaroop619 <agam.swaroop619@gmail.com>
2024-04-12 12:43:06 +0530

    add: immer code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import { Map } from "immutable";
-let book = Map({ title: "Harry Potter" });
+// import { Map } from "immutable";
+// let book = Map({ title: "Harry Potter" });
+
+// function publish(book) {
+//   return book.set("isPublished", true);
+// }
+
+// book = publish(book);
+
+// console.log(book.toJS());
+import { produce } from "immer";
+
+let book = { name: "Harry Potter" };
 
 function publish(book) {
-  return book.set("isPublished", true);
+  return produce(book, (draftBook) => {
+    draftBook.isPublished = true;
+  });
 }
 
-book = publish(book);
+let updated = publish(book);
 
-console.log(book.toJS());
+console.log(book);
+console.log(updated);
